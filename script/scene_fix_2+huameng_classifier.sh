c42401ccd24515cb7331b3395e2218a2147d60df
Da Xiao <xiaoda99@gmail.com>
2020-06-10 11:39:53 +0800

    Add by xd.

--- script/scene_fix_2+huameng_classifier.sh
+++ script/scene_fix_2+huameng_classifier.sh
@@ -3,36 +3,37 @@
 if [ "$1" = "train" ]; then
         echo "start to train......"
 
-        CUDA_VISIBLE_DEVICES=0,1 python ../scene_classifier/scene_classifier_train.py \
+        CUDA_VISIBLE_DEVICES=0,1 python ../scene_classifier/fix_2_train.py \
                                     --vocab_file /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/vocab.txt    \
                                     --bert_config_file /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/bert_config.json   \
                                     --do_lower_case    \
-                                    --train_file /nas/xd/projects/novel_analyzer/scene_cut_datas/data.train \
-                                    --eval_train_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/train_data.dev  \
-                                    --eval_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/data.dev   \
-                                    --train_batch_size 40   \
-                                    --eval_batch_size 40 \
-                                    --learning_rate 3e-5   \
-                                    --num_train_epochs 6   \
+                                    --train_file /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/data.train+huameng \
+                                    --eval_train_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/train_data.dev  \
+                                    --eval_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/data.dev   \
+                                    --train_batch_size 32   \
+                                    --eval_batch_size 32 \
+                                    --learning_rate 2e-5   \
+                                    --num_train_epochs 3   \
                                     --top_n 5   \
+                                    --max_seq_len 150 \
                                     --num_labels 2   \
-                                    --output_dir ./5_scene_model0527_bert \
+                                    --output_dir ../models/5_scene_model_bert_fix2+huameng \
                                     --bert_model /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese   \
                                     --init_checkpoint /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/pytorch_model.bin   \
                                     --do_train   \
-                                    --gradient_accumulation_steps 1 3>&2 2>&1 1>&3 | tee logs/5_scene_model0527_bert.log
+                                    --gradient_accumulation_steps 1 # 3>&2 2>&1 1>&3 | tee logs/3_scene_model0521_bert.log
 
 
 elif [ "$1" = "eval" ];then
         echo "start to eval......"
 
-        CUDA_VISIBLE_DEVICES=0,1 python ../scene_classifier/scene_classifier_train.py \
+        CUDA_VISIBLE_DEVICES=0,1 python ../scene_classifier/fix_3_train.py \
                                     --vocab_file /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/vocab.txt    \
                                     --bert_config_file /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/bert_config.json   \
                                     --do_lower_case    \
-                                    --train_file /nas/xd/projects/novel_analyzer/scene_cut_datas/small.data.train \
-                                    --eval_train_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/small.train_data.dev  \
-                                    --eval_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/data.dev   \
+                                    --train_file /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/small.data.train \
+                                    --eval_train_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/small.train_data.dev  \
+                                    --eval_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/data.dev   \
                                     --train_batch_size 20   \
                                     --eval_batch_size 20 \
                                     --learning_rate 1e-5   \
@@ -49,13 +50,13 @@ elif [ "$1" = "eval" ];then
 elif [ "$1" = "predict" ];then
         echo "start to predict......"
 
-        CUDA_VISIBLE_DEVICES=7,4 python ../scene_classifier/scene_classifier_train.py \
+        CUDA_VISIBLE_DEVICES=7,4 python ../scene_classifier/fix_2_train.py \
                                     --vocab_file /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/vocab.txt    \
                                     --bert_config_file /nas/pretrain-bert/pretrain-pytorch/bert-base-chinese/bert_config.json   \
                                     --do_lower_case    \
-                                    --train_file /nas/xd/projects/novel_analyzer/scene_cut_datas/small.data.train \
-                                    --eval_train_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/small.train_data.dev  \
-                                    --eval_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/small.data.dev   \
+                                    --train_file /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/small.data.train \
+                                    --eval_train_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/small.train_data.dev  \
+                                    --eval_file  /nas/xd/projects/novel_analyzer/scene_cut_datas/0527/small.data.dev   \
                                     --predict_file /nas/xd/data/kuaidian/clear_kuaidian_0519/高冷校草缠上我.txt \
                                     --train_batch_size 20   \
                                     --eval_batch_size 20 \
